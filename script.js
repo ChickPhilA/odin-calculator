5968202f9bc8b59933a57181dd9b8f4f056e5447
Phil <natsudragoneel282003@gmail.com>
2025-10-03 11:49:11 -0700

    created basic operation functions and operate function

---
+++ script.js
@@ -0,0 +1,39 @@
+let num1 = 0
+let num2 = 0
+let operator = ""
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    // Case to handle dividing by zero
+    if(a / b == 0) {
+        return "Error"
+    }
+
+    return a / b
+}
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case "+":
+            return add(a, b)
+        case "-":
+            return subtract(a, b)
+        case "*":
+            return multiply(a, b)
+        case "/":
+            return divide(a, b)
+        default:
+            return null
+    }
+}
